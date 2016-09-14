6e92e26f03449cd31a3b7fc1b3e56117e22a7364
Jason Brogan <jkbrogan@gmail.com>
2016-09-14 15:56:09 -0400

    HW1
    new hw1

--- hw1/hw1email.py
+++ hw1/hw1email.py
@@ -11,25 +11,26 @@ if breakfast == "oats":
 	question = raw_input("Did you run out of bananas again? y/n: ")
 	if question == "y":
 		print("You ran out of bananas again! Sending you a reminder now.")
-		
-		fromaddr = "brogj411@newschool.edu"
-		toaddr = "brogj411@newschool.edu"
-		msg = MIMEMultipart()
-		msg['From'] = fromaddr
-		msg['To'] = toaddr
-		msg['Subject'] = "BANANAS"
-		
-		body = "Jason, don't forget to buy bananas."
-		msg.attach(MIMEText(body, 'plain'))
-		
-		server = smtplib.SMTP('smtp.gmail.com', 587)
-		server.starttls()
-		server.login(fromaddr, "***PASSWORD***")
-		text = msg.as_string()
-		server.sendmail(fromaddr, toaddr, text)
-		server.quit()
-
+		emailMyself()
 	else:
 		print("You didn't run out of bananas this time!")
 else:
 	print("Yum!")
+
+def emailMyself():
+	fromaddr = "brogj411@newschool.edu"
+	toaddr = "brogj411@newschool.edu"
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = "BANANAS"
+	
+	body = "Jason, don't forget to buy bananas."
+	msg.attach(MIMEText(body, 'plain'))
+	
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(fromaddr, "PASS")
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
